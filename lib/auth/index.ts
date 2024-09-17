9e0f33afbf79446ed768213b4f0859905d9cd696
Deddy Wolley <deddywolley@gmail.com>
2024-09-17 12:51:51 +0800

    add github provider

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,5 +1,29 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    GitHub({
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          username: profile.login,
+          image: profile.avatar_url,
+        };
+      },
+    }),
+  ],
 });
+
+declare module "next-auth" {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      id: string;
+      username: string;
+      image: string;
+    } & DefaultSession["user"];
+  }
+}
